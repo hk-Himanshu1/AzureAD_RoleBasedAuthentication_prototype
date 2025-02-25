797b2d3d214aee6103ad7d1a434310d4b7d00e3d
HimanshuKumar-OSD <HimanshuKumar-OSD@WORKSPA-HKUMAR>
2025-02-25 15:29:16 +0530

    Add project files.

---
+++ AzureAD_RoleBasedAuthentication_prototype/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureAD_RoleBasedAuthentication_prototype.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
