7177946bcc0a8cf1e86ee229027ae9cb4365d750
Himanshu Kumar <hkumar@vohraphysicians.com>
2025-02-26 20:35:59 +0530

    from the hkumar change in the controller

---
+++ AzureAD_RoleBasedAuthentication_prototype/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureAD_RoleBasedAuthentication_prototype.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: LoginController1
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        // GET: LoginController1/Details/5
+        public ActionResult SignUp(int id)
+        {
+            return View();
+        }
+
+    }
+}
